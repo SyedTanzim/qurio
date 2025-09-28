2876ad2c51446b1791e2bd76303ada5e7eff9117
SyedTanzim <syedtanzimwajih@gmail.com>
2025-09-28 17:18:54 +0530

    feat: add home page to view posts and create new post functionality

--- index.js
+++ index.js
@@ -5,10 +5,40 @@ const port = 1920;
 
 app.set('views', path.join(__dirname , 'views'));
 app.set('view engine' , 'ejs');
-app.set(express.static(path.join(__dirname , 'public')));
 
-app.use(express.urlencoded({extends : true}));
+app.use(express.static(path.join(__dirname , 'public')));
+app.use(express.urlencoded({extended : true}));
 app.use(express.json());
+
+let posts = [
+    {
+        userName : 'Tanzim',
+        content : 'Work smart not hard'
+    },
+    {
+        userName : 'Junaid',
+        content : 'Does height matter'
+    },
+    {
+        userName : 'Almadni',
+        content : 'I am super gay'
+    }
+];
+
+app.get('/posts' , (req, resp) => {
+    resp.render('index.ejs', {posts});
+});
+
+app.get('/posts/new' , (req, resp) => {
+    resp.render('new.ejs')
+});
+
+app.post('/posts' , (req, resp) => {
+    let {userName , content}  = req.body;
+    posts.push({userName, content});
+    resp.redirect('/posts') 
+});
+
 
 app.get('/' , (req, resp) => {
     resp.send('everything is working fine');
